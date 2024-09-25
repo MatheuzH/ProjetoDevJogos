ec621c5c88f8d24e4c4d9b3b10822e94f1cebbf1
MatheuzH <matheusgadelha01@gmail.com>
2024-09-25 14:17:55 -0300

    transição de telas

--- Menus/escolha_de_mapa.gd
+++ Menus/escolha_de_mapa.gd
@@ -1,5 +1,13 @@
 extends Control
 
+var selected_index: int = -1  # Variável para armazenar o índice selecionado
+
+# Mapeia os índices para os arquivos de cena correspondentes
+var map_files = [
+	"res://mapas/flat/flat.tscn",
+	"res://mapas/biplat/bi_plat.tscn",
+	"res://mapas/triplat/tri_plat.tscn"
+]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,7 +18,13 @@ func _ready():
 func _process(delta):
 	pass
 
+func _on_item_list_item_clicked(index: int, at_position: Vector2, mouse_button_index: int) -> void:
+	selected_index = index  # Armazena o índice do item clicado
+	print("Item selecionado: ", selected_index)  # Exibe o item selecionado no console (opcional)
 
-func _on_map_selector_button_pressed():
-	get_tree().change_scene_to_file("res://mapas/flat/flat.tscn")
-	pass # Replace with function body.
+
+func _on_map_selector_button_pressed() -> void:
+	if selected_index >= 0 and selected_index < map_files.size():
+		get_tree().change_scene_to_file(map_files[selected_index])
+	else:
+		print("Nenhum item foi selecionado ou o índice está fora do alcance.")
--- Menus/tela_de_inicio.gd
+++ Menus/tela_de_inicio.gd
@@ -17,3 +17,7 @@ func _on_start_button_pressed() -> void:
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit() 
+
+
+func _on_creditos_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Menus/Creditos.tscn")
